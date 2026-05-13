7f5307fa4e55eecf354775c678d9ec5908ab7c66
madeleine <2198248606@qq.com>
2026-05-13 11:48:07 +0800

    add01: update same setting as 02

--- subproject/add/add01-tdep/src/main.cpp
+++ subproject/add/add01-tdep/src/main.cpp
@@ -28,10 +28,14 @@ struct VarConf{
 	TString yname;
 	TString yerrname;
 	TString ytitle;
-	double ymin;
-	double ymax;
+	bool has_xmin;
+	bool has_xmax;
+	double xmin;
+	double xmax;
 	bool has_ymin;
 	bool has_ymax;
+	double ymin;
+	double ymax;
 	TString foutname;
 	TString foutname_root;
 	TString foutname_pdf;
@@ -45,7 +49,7 @@ struct VarConf{
 
 //==================================================== INIT_
 void INIT(int argc, char *argv[], VarConf &var){
-	//---- 260421 default cfg for tfit
+	//====init var
 	var.fpath = "datain/26*/dataiss/tfit/";
 	var.fname = "fitresult_enebin03_1.33GeV.root";
 	var.fpathname = ""; // Initialize fpathname as an empty string
@@ -54,23 +58,21 @@ void INIT(int argc, char *argv[], VarConf &var){
 	var.yname = "nele";
 	var.yerrname = "nele_err";
 	var.ytitle = "Electron Number";
-	var.ymin = 0.0;
-	var.ymax = 0.0;
+	var.has_xmin = false;
+	var.has_xmax = false;
+	var.xmin = 1305849600;
+	var.xmax = 1761955200;
 	var.has_ymin = false;
 	var.has_ymax = false;
+	var.ymin = 0.0;
+	var.ymax = 0.0;
 	var.foutname = "htime";
 	var.elow = -1.0;
 	var.eup = -1.0;
-
-	// const double width_tbin = 60*60*24;  //---- 86400[s]
-	// static const int nt=6000;
-	// const double tmin = 1305417600;      //---- Sun May 15 2011 00:00:00 GMT+0000
-	// const double tmax = 1823817600;
 	var.nt = 6000;
 	var.tmin = 1305417600;
 	var.tmax = 1823817600;
-
-	//---- argv override (保持简单)
+	//====get
 	if(argc > 1) var.fpath = argv[1];
 	if(argc > 2) var.fname = argv[2];
 	if(argc > 3) var.fpathname = argv[3];
@@ -84,19 +86,26 @@ void INIT(int argc, char *argv[], VarConf &var){
 	if(argc > 11) var.tmin = atoi(argv[11]);
 	if(argc > 12) var.tmax = atoi(argv[12]);
 	if(argc > 13 && argv[13][0] != '\0'){
-		var.ymin = atof(argv[13]);
-		var.has_ymin = true;
+		var.xmin = atof(argv[13]);
+		var.has_xmin = true;
 	}
 	if(argc > 14 && argv[14][0] != '\0'){
-		var.ymax = atof(argv[14]);
+		var.xmax = atof(argv[14]);
+		var.has_xmax = true;
+	}
+	if(argc > 15 && argv[15][0] != '\0'){
+		var.ymin = atof(argv[15]);
+		var.has_ymin = true;
+	}
+	if(argc > 16 && argv[16][0] != '\0'){
+		var.ymax = atof(argv[16]);
 		var.has_ymax = true;
 	}
-
-	//---- name + suffix
+	//====process
 	if(var.fpathname.Length() == 0) var.fpathname = var.fpath + var.fname;
 	var.foutname_root = var.foutname + ".root";
 	var.foutname_pdf = var.foutname + ".pdf";
-
+	//====print
 	cout<<"IN INIT_conf ===== fpath="<<var.fpath
 		<<" fname="<<var.fname
 		<<" fpathname="<<var.fpathname
@@ -105,6 +114,8 @@ void INIT(int argc, char *argv[], VarConf &var){
 		<<" y="<<var.yname
 		<<" yerr="<<var.yerrname
 		<<" ytitle="<<var.ytitle
+		<<" xmin="<<(var.has_xmin ? Form("%g", var.xmin) : TString("N/A"))
+		<<" xmax="<<(var.has_xmax ? Form("%g", var.xmax) : TString("N/A"))
 		<<" ymin="<<(var.has_ymin ? Form("%g", var.ymin) : TString("N/A"))
 		<<" ymax="<<(var.has_ymax ? Form("%g", var.ymax) : TString("N/A"))
 		<<" outbase="<<var.foutname
@@ -116,44 +127,35 @@ void INIT(int argc, char *argv[], VarConf &var){
 
 //==================================================== FITHIST_
 void HIST(VarConf &var, TH1D *h){
-	int xvalue = 0;
-	double yvalue = 0.0;
-	double yerrvalue = 0.0;
+	//====init var
+	int x = 0;
+	double y = 0.0;
+	double yerr = 0.0;
 	double elow = -1.0;
 	double eup = -1.0;
-	bool has_yerr = false;
+	//====init chain
 	TChain chain(var.tname);
 	chain.Add(var.fpathname);
-	chain.SetBranchAddress(var.xname, &xvalue);
-	chain.SetBranchAddress(var.yname, &yvalue);
-	if(chain.GetBranch(var.yerrname)){
-		chain.SetBranchAddress(var.yerrname, &yerrvalue);
-		has_yerr = true;
-	}
-
-	//---- elow/eup branch: lowercase first, fallback to legacy uppercase
-	if(chain.GetBranch("elow")) chain.SetBranchAddress("elow", &elow);
-	else if(chain.GetBranch("Elow")) chain.SetBranchAddress("Elow", &elow);
-	if(chain.GetBranch("eup")) chain.SetBranchAddress("eup", &eup);
-	else if(chain.GetBranch("Eup")) chain.SetBranchAddress("Eup", &eup);
-
+	//====setbranch
+	chain.SetBranchAddress(var.xname, &x);
+	chain.SetBranchAddress(var.yname, &y);
+	chain.SetBranchAddress(var.yerrname, &yerr);
+	chain.SetBranchAddress("Elow", &elow);
+	chain.SetBranchAddress("Eup", &eup);
+	//====fill hist
 	int nx = chain.GetEntries();
 	for(int ix=0; ix<nx; ix++){
 		chain.GetEntry(ix);
-		h->Fill((double)xvalue, yvalue);
-		if(has_yerr){
-			int ibin = h->FindBin((double)xvalue);
-			h->SetBinError(ibin, yerrvalue);
-		}
+		h->Fill((double)x, y);
+		h->SetBinError(h->FindBin((double)x), yerr);
 	}
-	//---- write back to config (single-file path uses one energy bin)
 	var.elow = elow;
 	var.eup = eup;
+	//====print
 	cout<<"IN HIST ===== pattern="<<var.fpathname
 		<<" n_entry_total="<<nx
 		<<" elow="<<var.elow
 		<<" eup="<<var.eup
-		<<" has_yerr="<<has_yerr
 		<<" h_sum="<<h->Integral()
 		<<endl;
 }
@@ -161,17 +163,14 @@ void HIST(VarConf &var, TH1D *h){
 
 //==================================================== FITDRAW_
 void DRAW(const VarConf &var, TH1D *h){
-	const int x_unix_low = 1305849600;
-	// const int x_unix_up  = 1635811200;
-	const int x_unix_up  = 1761955200;
-
+	//====init--output
 	TFile *fout = TFile::Open(var.foutname_root, "RECREATE");
 	h->Write();
-
+	//====init--canvas
 	TCanvas *c = new TCanvas("c","c",1000,600);
 	TAxis *xaxis = h->GetXaxis();
 	TAxis *yaxis = h->GetYaxis();
-
+	//====canvas
 	h->SetStats(0);
 	h->SetNameTitle("", "");
 	c->SetTopMargin(0.13);
@@ -181,8 +180,9 @@ void DRAW(const VarConf &var, TH1D *h){
 	c->cd();
 	gPad->SetGridx();
 	gPad->SetGridy();
-
-	xaxis->SetRangeUser(x_unix_low, x_unix_up);
+	//====x
+	if(var.has_xmin && var.has_xmax) xaxis->SetRangeUser(var.xmin, var.xmax);
+	if(!var.has_xmin && !var.has_xmax) xaxis->SetRangeUser(var.xmin, var.xmax);
 	xaxis->SetNameTitle("Date", "Date");
 	xaxis->CenterTitle();
 	xaxis->SetTitleFont(62);
@@ -193,7 +193,7 @@ void DRAW(const VarConf &var, TH1D *h){
 	xaxis->SetTimeDisplay(1);
 	xaxis->SetTimeFormat("%b/%d/%Y");
 	xaxis->SetNdivisions(-505);
-
+	//====y
 	yaxis->SetNameTitle("expstime", var.ytitle);
 	yaxis->CenterTitle();
 	yaxis->SetTitleFont(62);
@@ -206,12 +206,11 @@ void DRAW(const VarConf &var, TH1D *h){
 	h->SetLineColor(kBlue);
 	if(var.has_ymin) h->SetMinimum(var.ymin);
 	if(var.has_ymax) h->SetMaximum(var.ymax);
-
+	//====draw
 	gStyle->SetEndErrorSize(0);
 	TGaxis::SetMaxDigits(3);
 	h->Draw("E1X0P");
-
-	//==================================================== add total title
+	//====draw title
 	c->cd(0);
 	TLatex latex;
 	latex.SetNDC();
@@ -220,16 +219,15 @@ void DRAW(const VarConf &var, TH1D *h){
 	latex.SetTextAlign(22);
 	if(var.elow>0 && var.eup>0) latex.DrawLatex(0.5, 0.97, Form("Energy %g to %g GeV", var.elow, var.eup));
 	else latex.DrawLatex(0.5, 0.97, "Energy N/A");
-
+	//====write
 	c->SaveAs(var.foutname_pdf);
 	fout->cd();
 	c->Write();
 	delete c;
-
 	fout->Write();
 	fout->Close();
 	delete fout;
-
+	//====print
 	cout<<"IN DRAW ===== output root: "<<var.foutname_root<<endl;
 	cout<<"IN DRAW ===== output pdf : "<<var.foutname_pdf<<endl;
 }
@@ -238,7 +236,7 @@ void DRAW(const VarConf &var, TH1D *h){
 
 //==================================================== FIT_RUN_
 int ADD_TIME(int argc, char *argv[]){
-	VarConf var;
+	VarConf var{};
 	INIT(argc, argv, var);
 	TH1D *htime = new TH1D("htime","htime",var.nt, var.tmin, var.tmax);
 	HIST(var, htime);
