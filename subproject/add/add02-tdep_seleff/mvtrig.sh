8632ab20817d752af879b2c44082bfa58166d30b
madeleine <2198248606@qq.com>
2026-05-19 10:19:02 +0800

    add02: add trig mv shell

---
+++ subproject/add/add02-tdep_seleff/mvtrig.sh
@@ -0,0 +1,7 @@
+# ============================================================ rm
+rm -r dataiss 
+# ============================================================ mkdir
+mkdir -p ./dataiss/trigeff/root
+# ============================================================ mv
+mv ./*trigeff.pdf ./dataiss/trigeff/
+mv ./*trigeff.root ./dataiss/trigeff/root/
